324bd4db0e12b702694a18d430e7ca3cc967abc7
Evgeny Prokhorov <evgeny.prokhorov@outlook.com>
2022-11-17 06:46:14 +0300

    Add seed data.

--- Frontends/Web/src/BusinessSolutions.Web.UI/Components/OrderEditorComponent.cshtml.cs
+++ Frontends/Web/src/BusinessSolutions.Web.UI/Components/OrderEditorComponent.cshtml.cs
@@ -3,6 +3,7 @@ using BusinessSolutions.Web.Application.ViewModels;
 using BusinessSolutions.Web.Application.Orders.Queries.GetOrder;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using BusinessSolutions.Web.Domain.Models;
 
 namespace BusinessSolutions.Web.UI.Components;
 
@@ -17,34 +18,33 @@ public class OrderEditorViewComponent : ViewComponent
         _mapper = mapper;
     }
 
-    public OrderWindowViewModel OrderViewModel { get; set; } = new();
+    public OrderWindowViewModel ViewModel { get; set; } = new();
 
     public async Task<IViewComponentResult> InvokeAsync(string? id, string? task)
     {
         if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(task))
         {
-            OrderViewModel = _mapper.Map<OrderWindowViewModel>(WindowViewModelFactory.Create(new OrderViewModel()));
+            ViewModel = _mapper.Map<OrderWindowViewModel>(WindowViewModelFactory.Create(new Order()));
         }
         else
         {
             var response = await _mediator.Send(new GetOrderQuery { Number = id });
             if (response is not null && !string.IsNullOrEmpty(task))
             {
-                var order = _mapper.Map<OrderViewModel>(response);
                 switch (task.ToUpperInvariant())
                 {
                     case "VIEW":
-                        OrderViewModel = _mapper.Map<OrderWindowViewModel>(WindowViewModelFactory.View(order));
+                        ViewModel = _mapper.Map<OrderWindowViewModel>(WindowViewModelFactory.View(response));
                         break;
                     case "EDIT":
-                        OrderViewModel = _mapper.Map<OrderWindowViewModel>(WindowViewModelFactory.Edit(order));
+                        ViewModel = _mapper.Map<OrderWindowViewModel>(WindowViewModelFactory.Edit(response));
                         break;
                     case "DELETE":
-                        OrderViewModel = _mapper.Map<OrderWindowViewModel>(WindowViewModelFactory.Delete(order));
+                        ViewModel = _mapper.Map<OrderWindowViewModel>(WindowViewModelFactory.Delete(response));
                         break;
                 };
             }
         }
-        return View(OrderViewModel);
+        return View(ViewModel);
     }
 }
